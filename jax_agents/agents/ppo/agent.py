01b0504c70c194e2514c95570e0c7d2aeede1d1f
Felipe Martins <felipemartins96@gmail.com>
2021-09-28 11:53:55 -0300

    🚧(PPO): Initial agent architecture

---
+++ jax_agents/agents/ppo/agent.py
@@ -0,0 +1,19 @@
+from jax_agents.agents.ppo.hyperparameters import HyperparametersPPO
+
+
+class AgentPPO:
+    def __init__(self):
+        pass
+
+    def observe(self):
+        pass
+
+    def update(self):
+        pass
+
+    def sample_action(self):
+        pass
+
+    @staticmethod
+    def get_hyperparameters():
+        return HyperparametersPPO()
---
+++ jax_agents/agents/ppo/hyperparameters.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class HyperparametersPPO:
+    algorithm_name: str = 'Proximal Policy Optimization'
