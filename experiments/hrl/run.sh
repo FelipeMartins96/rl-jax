43f0ee05f749d189b4b730ba0757e3907aaf7279
Felipe Martins <felipemartins96@gmail.com>
2022-02-01 00:14:50 -0300

    feat(experiments/hrl): more steps and new seed

--- experiments/hrl/run.sh
+++ experiments/hrl/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in 0 1 2; do
-  python train.py @experiments/base.txt @experiments/$i.txt
+  python train.py @experiments/base.txt @experiments/$i.txt @experiments/steps.txt
 done
